155421e771a9b6bcd430de2015a2d953a366d162
EmmanuelDonkor <edonkor0000@gmail.com>
2025-03-09 19:50:49 +0100

    updated: removed forgot passoword link

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -49,9 +49,6 @@ export default function Page() {
               </p>
             </div>
             <SignIn />
-            <Link href="/forgot-password" className="mt-4 block text-center text-sm text-gray-400 hover:text-gray-500 transition hover:underline">
-              Forgot Password?
-            </Link>
           </div>
         </main>
       </div>
